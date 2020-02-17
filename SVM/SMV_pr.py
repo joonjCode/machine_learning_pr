5b2b56b3baaa395782e61a7969d2052bcb05696c
joon <jtj308@naver.com>
2020-02-17 16:38:15 +0900

    Simple SVM

---
+++ SVM/SMV_pr.py
@@ -0,0 +1,28 @@
+import sklearn
+from sklearn import svm
+from sklearn import datasets
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import metrics
+
+cancer = datasets.load_breast_cancer()
+
+# print(cancer.feature_names)
+# print(cancer.target_names)
+
+x = cancer.data
+y = cancer.target
+
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.2)
+# print(x_train, y_train)
+
+classes = ['malignant', 'benign']
+
+clf = svm.SVC(kernel='linear', C=2)
+clf.fit(x_train, y_train)
+
+# predict
+y_pred = clf.predict(x_test)
+
+acc = metrics.accuracy_score(y_test, y_pred)
+
+print(acc)
