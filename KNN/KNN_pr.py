fb9c372db863edc4038dc117f020656cc2730387
joon <jtj308@naver.com>
2020-02-17 16:02:18 +0900

    Simple KNN Done

--- KNN/KNN_pr.py
+++ KNN/KNN_pr.py
@@ -6,7 +6,7 @@ import numpy as np
 from sklearn import linear_model, preprocessing
 
 data = pd.read_csv('car.data')
-print(data.head(5))
+# print(data.head(5))
 
 le = preprocessing.LabelEncoder()
 buying = le.fit_transform(list(data['buying']))
@@ -22,4 +22,17 @@ predict = 'class'
 X = list(zip(buying, maint, door, persons, lug_boot, safety))
 y = list(cls)
 
-x_train, x_text, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.1)
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.1)
+
+model = KNeighborsClassifier(n_neighbors=7)
+model.fit(x_train, y_train)
+acc = model.score(x_test, y_test)
+print('acc : ', acc)
+
+predicted = model.predict(x_test)
+names = ['unacc', 'acc', 'good', 'vgood']
+
+for x in range(len(predicted)):
+    print('Predicted : ', names[predicted[x]], 'Data : ', x_test[x], 'Actual : ', names[y_test[x]])
+    n = model.kneighbors([x_test[x]], 9, True)
+    print('N :', n)
